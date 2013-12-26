225f08645386d62a2bf46d82ad2b1114c8a05841
Icelle <icelleku@gmail.com>
2013-12-26 12:06:20 -0500

    initial commit, add rpsec, factory girl, valid attribute and shoulda

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DogPark::Application.config.secret_key_base = 'e5b3fe098ccbfe56c7a4b283dcf7d2bd76f400c521ddf63469d16e51172ca8f07b3bc05c696301a0a21dce87903f0a702e5e50feb46eb5f21f3152558a3f85f7'
